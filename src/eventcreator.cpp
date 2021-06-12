ab4ae1e2d87fc4d2c75f69ac20d45e4052e46adc
Claudio Cambra <claudio.cambra@gmail.com>
2021-06-13 00:48:36 +0200

    Added Q_PROPERTY and relevant functions for EventCreator

--- src/eventcreator.cpp
+++ src/eventcreator.cpp
@@ -13,3 +13,67 @@ EventCreator::~EventCreator()
 {
 
 }
+
+QString EventCreator::summary()
+{
+    return m_event->summary();
+}
+
+void EventCreator::setSummary(QString summary)
+{
+    m_event->setSummary(summary);
+}
+
+QString EventCreator::description()
+{
+    return m_event->description();
+}
+
+void EventCreator::setDescription(QString description)
+{
+    m_event->setDescription(description);
+}
+
+QDateTime EventCreator::eventStart()
+{
+    return m_event->dtStart();
+}
+
+void EventCreator::setEventStart(QDateTime eventStart)
+{
+    m_event->setDtStart(eventStart);
+}
+
+QDateTime EventCreator::eventEnd()
+{
+    return m_event->dtEnd();
+}
+
+void EventCreator::setEventEnd(QDateTime eventEnd)
+{
+    m_event->setDtStart(eventEnd);
+}
+
+KCalendarCore::Recurrence * EventCreator::recurrence()
+{
+    KCalendarCore::Recurrence *recurrence = m_event->recurrence();
+    return recurrence;
+}
+
+KCalendarCore::Attendee::List EventCreator::attendees()
+{
+    return m_event->attendees();
+}
+
+KCalendarCore::Alarm::List EventCreator::alarms()
+{
+    return m_event->alarms();
+}
+
+
+
+
+
+
+
+
--- src/eventcreator.h
+++ src/eventcreator.h
@@ -6,14 +6,41 @@
 #include <QObject>
 #include <CalendarSupport/KCalPrefs>
 #include <CalendarSupport/Utils>
+#include <AkonadiCore/CollectionIdentificationAttribute>
+
 
 class EventCreator : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString summary READ summary WRITE setSummary NOTIFY summaryChanged)
+    Q_PROPERTY(QString description READ description WRITE setDescription NOTIFY descriptionChanged)
+    Q_PROPERTY(QDateTime eventStart READ eventStart WRITE setEventStart NOTIFY eventStartChanged)
+    Q_PROPERTY(QDateTime eventEnd READ eventEnd WRITE setEventEnd NOTIFY eventEndChanged)
+    Q_PROPERTY(KCalendarCore::Recurrence * recurrence READ recurrence)
+    Q_PROPERTY(KCalendarCore::Attendee::List attendees READ attendees)
+    Q_PROPERTY(KCalendarCore::Alarm::List alarms READ alarms)
 
 public:
     EventCreator(QObject *parent = nullptr);
     ~EventCreator() override;
+
+    QString summary();
+    void setSummary(QString summary);
+    QString description();
+    void setDescription(QString description);
+    QDateTime eventStart();
+    void setEventStart(QDateTime eventStart);
+    QDateTime eventEnd();
+    void setEventEnd(QDateTime eventEnd);
+    KCalendarCore::Recurrence * recurrence();
+    KCalendarCore::Attendee::List attendees();
+    KCalendarCore::Alarm::List alarms();
+
+Q_SIGNALS:
+    void summaryChanged();
+    void descriptionChanged();
+    void eventStartChanged();
+    void eventEndChanged();
 
 private:
     KCalendarCore::Event::Ptr m_event;
