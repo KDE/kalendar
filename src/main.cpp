291e22170ad6792518a9801c352ded828979d683
Laurent Montel <montel@kde.org>
2021-12-23 07:56:53 +0100

    It's enabled by default in qt6

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,9 @@ static void raiseWindow(QWindow *window)
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("kalendar");
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
