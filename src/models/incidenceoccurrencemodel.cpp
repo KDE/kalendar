46e5731d441a95c621f4ee25e1776b97f5f711f2
Carl Schwan <carl@carlschwan.eu>
2023-05-18 20:19:03 +0200

    Use Akonadi::Collection::Id to store color keys
    This is faster than having a QString has index and remove some
    convertion from long to QString
    
    (cherry picked from commit 739710f76ed7ff805d141bd7e72f8a4f7435bc81)

--- src/models/incidenceoccurrencemodel.cpp
+++ src/models/incidenceoccurrencemodel.cpp
@@ -210,7 +210,7 @@ QColor IncidenceOccurrenceModel::getColor(const KCalendarCore::Incidence::Ptr &i
         return {};
     }
 
-    const QString id = QString::number(collection.id());
+    const auto id = collection.id();
 
     if (collection.hasAttribute<Akonadi::CollectionColorAttribute>()) {
         const auto colorAttr = collection.attribute<Akonadi::CollectionColorAttribute>();
@@ -356,8 +356,9 @@ void IncidenceOccurrenceModel::loadColors()
     const QStringList colorKeyList = rColorsConfig.keyList();
 
     for (const QString &key : colorKeyList) {
+        const auto keyId = key.toLong();
         QColor color = rColorsConfig.readEntry(key, QColor("blue"));
-        m_colors[key] = color;
+        m_colors[keyId] = color;
     }
 }
 
