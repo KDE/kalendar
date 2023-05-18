1939003b90e7f88c85e11693abf56ddebf746600
Claudio Cambra <claudio.cambra@kde.org>
2023-05-18 16:59:06 +0800

    Get collection color directly from collection when possible in IncidenceOccurrenceModel
    Signed-off-by: Claudio Cambra <claudio.cambra@kde.org>
    (cherry picked from commit 3b67e87d436bbad79325543f64aaa7ec8a190dca)

--- src/models/incidenceoccurrencemodel.cpp
+++ src/models/incidenceoccurrencemodel.cpp
@@ -10,6 +10,7 @@
 
 #include "../filter.h"
 #include "../utils.h"
+#include <Akonadi/CollectionColorAttribute>
 #include <Akonadi/EntityTreeModel>
 #include <KCalendarCore/OccurrenceIterator>
 #include <KConfigGroup>
@@ -195,19 +196,24 @@ qint64 IncidenceOccurrenceModel::getCollectionId(const KCalendarCore::Incidence:
 
 QColor IncidenceOccurrenceModel::getColor(const KCalendarCore::Incidence::Ptr &incidence)
 {
-    auto item = m_coreCalendar->item(incidence);
+    const auto item = m_coreCalendar->item(incidence);
     if (!item.isValid()) {
         return {};
     }
-    auto collection = item.parentCollection();
+    const auto collection = item.parentCollection();
     if (!collection.isValid()) {
         return {};
     }
-    const QString id = QString::number(collection.id());
-    // qDebug() << "Collection id: " << collection.id();
 
+    if (collection.hasAttribute<Akonadi::CollectionColorAttribute>()) {
+        const auto colorAttr = collection.attribute<Akonadi::CollectionColorAttribute>();
+        if (colorAttr && colorAttr->color().isValid()) {
+            return colorAttr->color();
+        }
+    }
+
+    const auto id = QString::number(collection.id());
     if (m_colors.contains(id)) {
-        // qDebug() << collection.id() << "Found in m_colors";
         return m_colors[id];
     }
 
