71d7fcf4c98c37abb67deb9faad4311c26811956
Laurent Montel <montel@kde.org>
2023-06-21 13:11:03 +0200

    Add missing #pragma once

--- src/filter.h
+++ src/filter.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Carl Schwan <carl@carlschwan.eu>
 // SPDX-License-Identifier: LGPL-2.0-or-later
-
+#pragma once
 #include <QObject>
 
 /**
--- src/mail/mailmanager.h
+++ src/mail/mailmanager.h
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2020 Carl Schwan <carlschwan@kde.org>
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
+#pragma once
+
 #include "mailmodel.h"
 #include <Akonadi/CollectionFilterProxyModel>
 #include <QObject>
